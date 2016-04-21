435a09a2ab51f02955e709503427170fdc117ec6
Shawn Catanzarite <me@shawncatz.com>
2016-04-20 19:03:43 -0700

    support hipchat responses (from hubot primarily)

--- lib/slnky/hipchat/service.rb
+++ lib/slnky/hipchat/service.rb
@@ -14,8 +14,13 @@ module Slnky
       # subscribe 'something.happened.*', :other_handler
 
       def run
-        transport.queue('hipchat', 'logs').subscribe do |raw|
+        logs = transport.queue('hipchat', 'logs')
+        logs.subscribe do |raw|
           handle_log(parse(raw))
+        end
+        response = transport.queue('hipchat', 'response', durable: false, auto_delete: true, routing_key: 'hipchat')
+        response.subscribe do |raw|
+          handle_response(parse(raw))
         end
       end
 
@@ -31,11 +36,25 @@ module Slnky
         return nil unless data.chat && data.chat.message
         room = data.chat.room
         message = data.chat.message
-        unless data.chat.format == 'html'
-          message = "<b>#{data.chat.message}</b><br/><i>event: #{name}</i>"
-          data.chat.format = 'html'
-        end
+        # unless data.chat.format == 'html'
+        #   message = "<b>#{data.chat.message}</b><br/><i>event: #{name}</i>"
+        #   data.chat.format = 'html'
+        # end
         client.hipchat_send(room, message, notify: data.chat.notify, color: data.chat.color, format: data.chat.format)
+      end
+
+      def handle_response(message)
+        (user, room) = message.reply.split(':', 2)
+        return if %w{start complete}.include?(message.level)
+        color = case message.level
+                  when 'info'
+                    'green'
+                  when 'warn'
+                    'yellow'
+                  when 'error'
+                    'red'
+                end
+        client.hipchat_send(room, message.message, notify: true, color: color, format: 'text')
       end
     end
   end
